c7db2514480cc330a97358e79fd080f05949e9ad
Petar Totev <petar@petartotev.net>
2022-06-18 09:49:38 +0300

    Polly - 5 retries in a row, immediate retry

--- src/RequestService/Controllers/RequestController.cs
+++ src/RequestService/Controllers/RequestController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RequestService.Policies;
 
 namespace RequestService.Controllers
 {
@@ -6,14 +7,23 @@ namespace RequestService.Controllers
     [Route("api/v1/[Controller]")]
     public class RequestController : ControllerBase
     {
+        private readonly ClientPolicy _clientPolicy;
+
+        public RequestController(ClientPolicy clientPolicy)
+        {
+            _clientPolicy = clientPolicy;
+        }
+
         [HttpGet]
         public async Task<ActionResult> MakeRequest()
         {
-            var id = 50;
-            var url = $"https://localhost:6000/api/v1/response/{id}";
+            var id = 25;
+            var url = $"https://localhost:7000/api/v1/response/{id}";
             var client = new HttpClient();
 
-            var response = await client.GetAsync(url);
+            //var response = await client.GetAsync(url);
+
+            var response = await _clientPolicy.ImmediateHttpRetry.ExecuteAsync(() => client.GetAsync(url));
 
             if (response.IsSuccessStatusCode)
             {
---
+++ src/RequestService/Policies/ClientPolicy.cs
@@ -0,0 +1,17 @@
+﻿using Polly;
+using Polly.Retry;
+
+namespace RequestService.Policies
+{
+    public class ClientPolicy
+    {
+        public AsyncRetryPolicy<HttpResponseMessage> ImmediateHttpRetry { get; }
+
+        public ClientPolicy()
+        {
+            ImmediateHttpRetry = Policy
+                .HandleResult<HttpResponseMessage>(res => !res.IsSuccessStatusCode)
+                .RetryAsync(5);
+        }
+    }
+}
