d5bc37f2483a12ef0ea165705bc8cd289f664ce9
Petar Totev <petar@petartotev.net>
2022-06-18 10:03:35 +0300

    Polly - 5 retries in a row, linear retry with wait-and-retry

--- src/RequestService/Controllers/RequestController.cs
+++ src/RequestService/Controllers/RequestController.cs
@@ -23,7 +23,7 @@ namespace RequestService.Controllers
 
             //var response = await client.GetAsync(url);
 
-            var response = await _clientPolicy.ImmediateHttpRetry.ExecuteAsync(() => client.GetAsync(url));
+            var response = await _clientPolicy.LinearHttpRetry.ExecuteAsync(() => client.GetAsync(url));
 
             if (response.IsSuccessStatusCode)
             {
--- src/RequestService/Policies/ClientPolicy.cs
+++ src/RequestService/Policies/ClientPolicy.cs
@@ -6,12 +6,17 @@ namespace RequestService.Policies
     public class ClientPolicy
     {
         public AsyncRetryPolicy<HttpResponseMessage> ImmediateHttpRetry { get; }
+        public AsyncRetryPolicy<HttpResponseMessage> LinearHttpRetry { get; }
 
         public ClientPolicy()
         {
             ImmediateHttpRetry = Policy
                 .HandleResult<HttpResponseMessage>(res => !res.IsSuccessStatusCode)
                 .RetryAsync(5);
+
+            LinearHttpRetry = Policy
+                .HandleResult<HttpResponseMessage>(res => !res.IsSuccessStatusCode)
+                .WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(3));
         }
     }
 }
